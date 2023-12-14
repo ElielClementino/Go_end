6bb75e7bf94536a7fc9165f9b813d54a19f01fc0
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-12-14 00:47:22 -0300

    Add logger

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/ElielClementino/Go_end/router"
+import (
+	"fmt"
+
+	"github.com/ElielClementino/Go_end/config"
+	"github.com/ElielClementino/Go_end/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	// Inicialize the router listening on :8080
+	logger = config.GetLogger("main")
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errf("Config initialization error: %v", err)
+		fmt.Println(err)
+		return
+	}
+
+	// Initialize the router listening on :8080
 	router.Initialize()
 }
